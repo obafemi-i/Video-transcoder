299c8dd28cc040e9773be7e18105c21091bc6e34
obafemi-i <obaff76@gmail.com>
2023-10-28 15:26:57 +0100

    anti docker

--- src/auth/jwt_tokens.py
+++ src/auth/jwt_tokens.py
@@ -1,14 +1,15 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 # from . schema import TokenData
-from .schema import TokenData
+import schema
 from dotenv import dotenv_values
 from typing import Optional
 
 config = dotenv_values()
 
 
-SECRET_KEY = config['SECRET_KEY']
+# SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = '09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7'
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -32,7 +33,7 @@ def verify_token(token: str, credentials_exception):
 
         if email is None:
             raise credentials_exception
-        token_data = TokenData(email=email)
+        token_data = schema.TokenData(email=email)
 
     except JWTError:
         raise credentials_exception
