bc9e6d69003399486679d7354f1d9be8211fd860
obafemi-i <obaff76@gmail.com>
2023-10-31 18:04:03 +0100

    gateway db connection

--- src/gateway/main.py
+++ src/gateway/main.py
@@ -1,3 +1,43 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from dotenv import dotenv_values
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+config = dotenv_values()
+# print(config['ATLAS_URI'])
+# uri = "mongodb+srv://Obafemi:obafemi@cluster1.esmjidp.mongodb.net/?retryWrites=true&w=majority"
+
+# print('uri', uri)
+
+# if config['ATLAS_URI'] == uri:
+#     print(True)
+
+def start_db():
+    try:
+        mongodb_client = MongoClient(config["ATLAS"])
+        print('Connected to mongodb database!')
+        database = mongodb_client[config['DB_NAME']]
+        
+    except Exception as e:
+        print(e)
+
+
+def close_db():
+    mongodb_client = MongoClient(config['ATLAS_URI'])
+    mongodb_client.close()
+    print('connection closed')
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    start_db()
+
+    yield
+
+    close_db()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
