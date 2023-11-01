cb8fa4261bf797205939307df2f4565a3402ece3
obafemi-i <obaff76@gmail.com>
2023-11-01 15:34:28 +0100

    gateway upload

--- src/gateway/main.py
+++ src/gateway/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, HTTPException, status, Request
 from pymongo import MongoClient
 from dotenv import dotenv_values
 import pika
@@ -33,24 +33,24 @@ def close_db():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # start_db()
-    mongodb_client = MongoClient(config['ATLAS_URI'])
+    app.mongodb_client = MongoClient(config['ATLAS_URI'])
+    app.database = app.mongodb_client[config['DB_NAME']]
     print('Connected to mongodb database!')
-    # database = mongodb_client[config['DB_NAME']]
 
     yield
-
-    mongodb_client.close()
-    print('connection closed')
-
     # close_db()
+
+    app.mongodb_client.close()
+    print('Connection closed')
 
 
 app = FastAPI(lifespan=lifespan)
 # app = FastAPI()
 
 
-# connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 # channel = connection.channel()
+# channel.queue_declare(queue='file_queue')
 
 @app.post('/access')
 def get_access(request: Login):
@@ -68,3 +68,11 @@ def get_access(request: Login):
 
     return response.json()
 
+@app.post('/upload')
+async def upload_file(request: Request,file: UploadFile = File(...)):
+    if not file:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail='No file provided')
+    
+    file_id = ''
+    print('reqqq',app.database)
