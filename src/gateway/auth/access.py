f692d891b78b0ffc806488f8b5bc9504fb2db2db
obafemi-i <obaff76@gmail.com>
2023-11-02 16:21:22 +0100

    gateway auth fix

--- src/gateway/auth/access.py
+++ src/gateway/auth/access.py
@@ -1,41 +1,34 @@
-from fastapi import FastAPI, HTTPException, Depends, Form, status
+from fastapi import APIRouter, HTTPException, Depends, Form, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import requests
 from jose import jwt, ExpiredSignatureError, JWTError
 
+router = APIRouter()
 
-app = FastAPI()
 
 # Secret key to sign and verify JWT tokens
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-AUTH_SERVICE_URL = "http://127.0.0.1:8080/login"  # Change this to the URL of your authentication service
+AUTH_SERVICE_URL = "http://127.0.0.1:8080/login" 
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
     
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-# def get_current_user(token: str = get_token_from_form):
     # Function to get the current user from the token
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         # email: str = payload.get("sub")
-        print(payload)
+        # print(payload)
         return payload.get('sub')
-        # token
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
     except JWTError:
         raise HTTPException(status_code=401, detail="Could not validate credentials")
 
 
-@app.get("/secure-endpoint")
-def secure_endpoint(current_user: dict = Depends(get_current_user)):
-    # Your secure endpoint logic goes here
-    return {"message": "This is a secure endpoint", "user": current_user}
-
-
-@app.post("/login")
+@router.post("/login")
 def login(request: OAuth2PasswordRequestForm = Depends()):
     # This endpoint will request authentication from the authentication service and return a token
     payload = {'username': request.username, 'password': request.password}
--- src/gateway/main.py
+++ src/gateway/main.py
@@ -2,16 +2,12 @@ from fastapi import FastAPI, File, UploadFile, HTTPException, status, Request, D
 from fastapi.background import BackgroundTasks
 from pymongo import MongoClient
 from dotenv import dotenv_values
-import pika
-import requests
 from redis_om import get_redis_connection
 
 import gridfs
 from contextlib import asynccontextmanager
+from auth.access import get_current_user, router
 
-from schema import Login
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, ExpiredSignatureError, JWTError
 
 config = dotenv_values()
 
@@ -21,6 +17,7 @@ redis_connect = get_redis_connection(
     password=config['PASSWORD'],
     decode_responses=True
 )
+
 
 def start_db():
     try:
@@ -56,51 +53,16 @@ async def lifespan(app: FastAPI):
 
 # app = FastAPI(lifespan=lifespan)
 app = FastAPI()
-
-
-# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-# channel = connection.channel()
-# channel.queue_declare(queue='file_queue')
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="access")
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
-    # Function to get the current user from the token
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        # email: str = payload.get("sub")
-        return payload
-        # token
-    except ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token has expired")
-    except JWTError:
-        raise HTTPException(status_code=401, detail="Could not validate credentials")
-    
-
-@app.post('/access')
-def get_access(request: Login):
-
-    url = 'http://127.0.0.1:8080/login'
-
-    data = request.username
-    passw = request.password
-
-    datad = {'username':data, 'password': passw}
-
-    response = requests.post(url, data=datad)
-
-    print('response', response.json()['access_token'])
-
-    return response.json()
+app.include_router(router)
 
 
 @app.post('/upload')
-async def upload_file(request: Request,file: UploadFile = File(...), current_user: dict = Depends(get_current_user)):
+async def upload_file(request: Request, file: UploadFile = File(...), current_user: dict = Depends(get_current_user)):
     if not file:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                             detail='No file provided.')
+    
+
     
     fs = gridfs.GridFS(request.app.database)
     try:
@@ -111,9 +73,10 @@ async def upload_file(request: Request,file: UploadFile = File(...), current_use
     
     queue_message = {
         'Video id': file_id,
-        'Mp3 id': None
+        'Mp3 id': None,
+        'User': current_user
     }
+
     redis_connect.xadd('video upload', queue_message, '*')
 
-    # print('reqqq',request.app.database)
-
+    return {'Message': f'Upload by {current_user} succesful.'}
