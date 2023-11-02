4d90fe0e00b57c97f3bf991bd79f37322fc6d9e9
obafemi-i <obaff76@gmail.com>
2023-11-02 15:23:16 +0100

    gateway 0auth2

--- src/gateway/auth/access.py
+++ src/gateway/auth/access.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, HTTPException, Depends, Form, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import requests
 from jose import jwt, ExpiredSignatureError, JWTError
-from pydantic import BaseModel
+
 
 app = FastAPI()
 
@@ -12,22 +12,22 @@ ALGORITHM = "HS256"
 AUTH_SERVICE_URL = "http://127.0.0.1:8080/login"  # Change this to the URL of your authentication service
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+    
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
+# def get_current_user(token: str = get_token_from_form):
     # Function to get the current user from the token
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         # email: str = payload.get("sub")
-        return payload
+        print(payload)
+        return payload.get('sub')
         # token
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
     except JWTError:
         raise HTTPException(status_code=401, detail="Could not validate credentials")
 
-class Login(BaseModel):
-    username: str
-    password: str
 
 @app.get("/secure-endpoint")
 def secure_endpoint(current_user: dict = Depends(get_current_user)):
@@ -36,16 +36,14 @@ def secure_endpoint(current_user: dict = Depends(get_current_user)):
 
 
 @app.post("/login")
-def login(request: Login):
+def login(request: OAuth2PasswordRequestForm = Depends()):
     # This endpoint will request authentication from the authentication service and return a token
-    # headers = {"Content-Type": "application/json"}
-    payload = {"username": request.username, "password": request.password}
+    payload = {'username': request.username, 'password': request.password}
 
-    response = requests.post(AUTH_SERVICE_URL, data=payload)
+    response = requests.post(AUTH_SERVICE_URL, payload)
     
     if response.status_code == 200:
         token = response.json().get("access_token")
-        # return {"access_token": token, "token_type": "bearer"}
 
         return {"access_token": token}
     else:
